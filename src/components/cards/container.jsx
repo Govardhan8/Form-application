0f0159520d62b6be7466099c521dc05cdbbd08f0
Govardhan8 <govardhanmurthy007@gmail.com>
2023-04-28 19:19:46 +0530

    Fix for card container

--- src/components/cards/container.jsx
+++ src/components/cards/container.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function container({ children, customCss }) {
+function container({ children }) {
   return (
     <div
-      className={`w-4/6 max-w-[830px] h-[320px] py-4 px-6 rounded-[10px] bg-primary-white flex justify-between`}
+      className={`w-5/6 max-w-[830px] min-h-[320px] py-4 px-6 rounded-[10px] bg-primary-white flex justify-between`}
     >
       {children}
     </div>
